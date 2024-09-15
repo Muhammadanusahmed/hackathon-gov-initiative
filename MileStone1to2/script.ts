6ad23743970a577f8a17515a4837e53ecf0e7c34
Muhammad_Anus_Ahmed <160781213+Muhammadanusahmed@users.noreply.github.com>
2024-09-15 14:58:36 +0500

    Update script.ts

--- MileStone1to2/script.ts
+++ MileStone1to2/script.ts
@@ -1,11 +1,11 @@
-let skillbtn = document.querySelector("button") as HTMLButtonElement
-let skilldat3 = document.getElementById("dat3") as HTMLElement
+let button = document.querySelector("button") as HTMLButtonElement
+let dat3 = document.getElementById("dat3") as HTMLElement
 
 
-skillbtn.addEventListener("click", function () {
-    if (skilldat3.style.display === "none") {
-        skilldat3.style.display = "block";  
+button.addEventListener("click", function () {
+    if (dat3.style.display === "none") {
+        dat3.style.display = "block";  
     } else {
-        skilldat3.style.display = "none";  
+        dat3.style.display = "none";  
     }
 });
