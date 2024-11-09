126aa2d5d895a10889dcadae356fcb33a7d9927d
Muhammad_Anus_Ahmed <160781213+Muhammadanusahmed@users.noreply.github.com>
2024-11-09 23:13:39 +0500

    milestone3
    milestone3 for assignment submitting

---
+++ MileStone3/script.js
@@ -0,0 +1,43 @@
+
+
+let Name = document.getElementById("Name").value;
+let Profile = document.getElementById("Profile");
+
+
+// check box
+let Urdu = document.getElementById("Urdu");
+let English = document.getElementById("English");
+let Arbi = document.getElementById("Arbi");
+let German = document.getElementById("German");
+let Franch = document.getElementById("Franch");
+let Chinese = document.getElementById("Chinese");
+
+let Location = document.getElementById("Location");
+let Phone = document.getElementById("Phone");
+let Email = document.getElementById("Email");
+let Linkdin = document.getElementById("Linkdin");
+let Github = document.getElementById("Github");
+
+let Passion = document.getElementById("Passion");
+let Education = document.getElementById("Education");
+let Experience = document.getElementById("Experience");
+let Skill = document.getElementById("Skill");
+
+let form = document.querySelector("form");
+
+let submitbtn = document.getElementById("submitbtn");
+
+form.addEventListener("submit", () => {
+    
+    // let name = document.createElement("h1");
+    // name.textContent = Name;
+    // name.replaceWith(document.querySelector("h1"))
+
+    console.log(Name);
+    
+    // const li2 = document.querySelector("h1")
+    // const newli = document.createElement("h1")
+    // newli.textContent = "typeScript"
+    // li2.replaceWith(newli)
+
+})
